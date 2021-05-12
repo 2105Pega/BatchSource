3af18662400ded625d90798f0d5d7ac16c6837e5
RMalonda <ronsardmalonda@live.com>
2021-05-12 14:28:00 -0400

    Added Assignment1

---
+++ Week1/Assignment1/src/main/java/com/revature/Driver.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+package com.revature;
+import com.revature.beans.*;
+
+/**
+ * @author Ronsard Malonda
+ *
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+
+	}
+
+}
